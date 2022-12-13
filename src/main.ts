88087d1d58451efb10298fdc1e370938397d4f4f
Mateus Santiago <mateus_santiago2.3@outlook.com>
2022-12-13 12:30:01 -0300

    feat: update main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('MovieFlix 2.0')
+    .setDescription('Aplicação de filmes, séries, desenhos e documentários')
+    .setVersion('2.0.0')
+    .addTag('Status')
+    .addTag('User')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3003);
 }
 bootstrap();

6cac73b75c3892153556eb6a5afb70fca2bedc63
Mateus Santiago <mateus_santiago2.3@outlook.com>
2022-12-13 12:23:24 -0300

    feat: update dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,11 +1,56 @@
+import { ApiProperty } from "@nestjs/swagger/dist";
+
 export class CreateUserDto {
+    @ApiProperty({
+        description: 'Nome do usuário',
+        example: 'Mateus Jacinto Santiago',
+    })    
     name: string;
+
+    @ApiProperty({
+        description: 'CPF do usuário',
+        example: '99999999999'
+    })
     cpf: string;
+
+    @ApiProperty({
+        description: 'Email do usuário',
+        example: 'exemplo@exemplo.com.br'
+    })
     email: string;
+
+    @ApiProperty({
+        description: 'Telefone para contrato',
+        example: 999999999,
+    })
     contato: number;
+
+    @ApiProperty({
+        description: 'Idade de usuário',
+        example: 20,
+    })
     idade: number;
+
+    @ApiProperty({
+        description: 'Cidade',
+        example: 'São Paulo'
+    })
     cidade: string;
+
+    @ApiProperty({
+        description: 'Permissão de acesso',
+    })
     role: string;
+
+    @ApiProperty({
+        description: 'Senha do usuário',
+        example: 'secret'
+    })
     password: string;
+
+    @ApiProperty({
+        description: 'Confirmação da senha',
+        example: 'secret'
+    })
     confirmPassword: string;
 }

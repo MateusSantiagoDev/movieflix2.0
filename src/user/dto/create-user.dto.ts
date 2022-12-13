6b57f67a0c996e3d6d6ef99e889c640eb1f04c0d
Mateus Santiago <mateus_santiago2.3@outlook.com>
2022-12-13 12:08:11 -0300

    feat(user): created user method

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,11 @@
+export class CreateUserDto {
+    name: string;
+    cpf: string;
+    email: string;
+    contato: number;
+    idade: number;
+    cidade: string;
+    role: string;
+    password: string;
+    confirmPassword: string;
+}
---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,11 @@
+export class UserEntity {
+  id: string;
+  name: string;
+  cpf: string;
+  email: string;
+  contato: number;
+  idade: number;
+  cidade: string;
+  role: string;
+  password: string;
+}

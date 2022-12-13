c44d9cc41cd41fd7a559d69fce88e3dfaa53cbf8
Mateus Santiago <mateus_santiago2.3@outlook.com>
2022-12-13 14:40:19 -0300

    feat(user): updated prisma and implemented repository method

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -1,22 +1,28 @@
-import { Injectable } from "@nestjs/common";
-import { UserEntity } from "./entities/user.entity";
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UserEntity } from './entities/user.entity';
 
 @Injectable()
 export class UserRepository {
-    delete(id: string): UserEntity | PromiseLike<UserEntity> {
-        throw new Error("Method not implemented.");
-    }
-    update(id: string, data: Partial<UserEntity>): UserEntity | PromiseLike<UserEntity> {
-        throw new Error("Method not implemented.");
-    }
-    findOne(id: any): Promise<UserEntity> {
-        throw new Error("Method not implemented.");
-    }
-    findAll(): Promise<UserEntity[]> {
-        throw new Error("Method not implemented.");
-    }
-    create(data: UserEntity): Promise<import("./entities/user.entity").UserEntity> {
-        throw new Error("Method not implemented.");
-    }
-    constructor() {}
+  constructor(private readonly prisma: PrismaService) {}
+
+  create(data: UserEntity): Promise<UserEntity> {
+    return this.prisma.user.create({ data });
+  }
+
+  findAll(): Promise<UserEntity[]> {
+    return this.prisma.user.findMany();
+  }
+
+  findOne(id: string): Promise<UserEntity> {
+    return this.prisma.user.findUniqueOrThrow({ where: { id } });
+  }
+
+  update(id: string, data: Partial<UserEntity>): Promise<UserEntity> {
+    return this.prisma.user.update({ where: { id }, data });
+  }
+
+  delete(id: string): Promise<UserEntity> {
+    return this.prisma.user.delete({ where: { id } });
+  }
 }

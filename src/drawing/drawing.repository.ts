5513211eff967d7b9474f34b0d9d58662b6128be
Mateus Santiago <mateus_santiago2.3@outlook.com>
2022-12-14 11:49:45 -0300

    feat(drawing): implemented repository methods

--- src/drawing/drawing.repository.ts
+++ src/drawing/drawing.repository.ts
@@ -1,7 +1,28 @@
-import { Injectable } from "@nestjs/common";
-import { PrismaService } from "src/prisma/prisma.service";
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { DrawingEntity } from './entities/drawing.entity';
 
 @Injectable()
 export class DrawingRepository {
-    constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService) {}
+
+  create(data: DrawingEntity): Promise<DrawingEntity> {
+    return this.prisma.drawing.create({ data });
+  }
+
+  findAll(): Promise<DrawingEntity[]> {
+    return this.prisma.drawing.findMany();
+  }
+
+  findOne(id: string): Promise<DrawingEntity> {
+    return this.prisma.drawing.findUnique({ where: { id } });
+  }
+
+  update(id: string, data: Partial<DrawingEntity>): Promise<DrawingEntity> {
+    return this.prisma.drawing.update({ where: { id }, data });
+  }
+
+  delete(id: string): Promise<DrawingEntity> {
+    return this.prisma.drawing.delete({ where: { id } });
+  }
 }
--- src/drawing/drawing.service.ts
+++ src/drawing/drawing.service.ts
@@ -1,7 +1,45 @@
-import { Injectable } from "@nestjs/common";
-import { DrawingRepository } from "./drawing.repository";
+import { Injectable } from '@nestjs/common';
+import { randomUUID } from 'crypto';
+import { DrawingRepository } from './drawing.repository';
+import { CreateDrawingDto } from './dto/create-drawing.dto';
+import { UpdateDrawingDto } from './dto/update-drawing.dto';
+import { DrawingEntity } from './entities/drawing.entity';
 
 @Injectable()
 export class DrawingService {
-    constructor(private readonly repository: DrawingRepository) {}
+  constructor(private readonly repository: DrawingRepository) {}
+
+  create(dto: CreateDrawingDto): Promise<DrawingEntity> {
+    try {
+      const data: DrawingEntity = { ...dto, id: randomUUID() };
+      return this.repository.create(data);
+    } catch (err) {}
+  }
+
+  findAll(): Promise<DrawingEntity[]> {
+    try {
+      return this.repository.findAll();
+    } catch (err) {}
+  }
+
+  findOne(id: string): Promise<DrawingEntity> {
+    try {
+      return this.repository.findOne(id);
+    } catch (err) {}
+  }
+
+  async update(id: string, dto: UpdateDrawingDto): Promise<DrawingEntity> {
+    try {
+      await this.findOne(id);
+      const data: Partial<DrawingEntity> = { ...dto };
+      return await this.repository.update(id, data);
+    } catch (err) {}
+  }
+
+  async delete(id: string): Promise<DrawingEntity> {
+    try {
+      await this.findOne(id);
+      return await this.repository.delete(id);
+    } catch (err) {}
+  }
 }

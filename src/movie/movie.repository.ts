71561c5c2b71d1a9ea50466bc88729a8eb1fae70
Mateus Santiago <mateus_santiago2.3@outlook.com>
2022-12-13 22:02:15 -0300

    feat(movie): create service method

--- src/movie/movie.repository.ts
+++ src/movie/movie.repository.ts
@@ -1,6 +1,22 @@
 import { Injectable } from "@nestjs/common";
+import { MovieEntity } from "./entities/movie-entity";
 
 @Injectable()
 export class MovieRepository {
+    delete(id: string): Promise<MovieEntity> {
+        throw new Error("Method not implemented.");
+    }
+    update(data: Partial<MovieEntity>): MovieEntity | PromiseLike<MovieEntity> {
+        throw new Error("Method not implemented.");
+    }
+    findOne(id: string): Promise<MovieEntity> {
+        throw new Error("Method not implemented.");
+    }
+    findAll(): Promise<MovieEntity[]> {
+        throw new Error("Method not implemented.");
+    }
+    create(data: MovieEntity): Promise<import("./entities/movie-entity").MovieEntity> {
+        throw new Error("Method not implemented.");
+    }
     constructor() {}
 }
--- src/movie/movie.service.ts
+++ src/movie/movie.service.ts
@@ -1,22 +1,45 @@
-import { Injectable } from "@nestjs/common";
-import { UpdateMovieDto } from "./dto/update-movie.dto";
+import { Injectable } from '@nestjs/common';
+import { randomUUID } from 'crypto';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
+import { MovieEntity } from './entities/movie-entity';
+import { MovieRepository } from './movie.repository';
 
 @Injectable()
 export class MovieService {
-    delete(id: string): Promise<import("./entities/movie-entity").MovieEntity> {
-        throw new Error("Method not implemented.");
-    }
-    update(id: string, dto: UpdateMovieDto): Promise<import("./entities/movie-entity").MovieEntity> {
-        throw new Error("Method not implemented.");
-    }
-    findOne(id: string): Promise<import("./entities/movie-entity").MovieEntity> {
-        throw new Error("Method not implemented.");
-    }
-    findAll(): Promise<import("./entities/movie-entity").MovieEntity> {
-        throw new Error("Method not implemented.");
-    }
-    create(dto): Promise<import("./entities/movie-entity").MovieEntity> {
-        throw new Error("Method not implemented.");
-    }
-    constructor() {}
+  constructor(private readonly repository: MovieRepository) {}
+
+  create(dto: CreateMovieDto): Promise<MovieEntity> {
+    try {
+      const data: MovieEntity = { ...dto, id: randomUUID() };
+      return this.repository.create(data);
+    } catch (err) {}
+  }
+
+  findAll(): Promise<MovieEntity[]> {
+    try {
+      return this.repository.findAll();
+    } catch (err) {}
+  }
+
+  findOne(id: string): Promise<MovieEntity> {
+    try {
+      return this.repository.findOne(id);
+    } catch (err) {}
+  }
+
+  async update(id: string, dto: UpdateMovieDto): Promise<MovieEntity> {
+    try {
+      await this.findOne(id);
+      const data: Partial<MovieEntity> = { ...dto };
+      return await this.repository.update(data);
+    } catch (err) {}
+  }
+
+  async delete(id: string): Promise<MovieEntity> {
+    try {
+      await this.findOne(id);
+      return await this.repository.delete(id);
+    } catch (err) {}
+  }
 }

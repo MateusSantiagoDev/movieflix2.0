9c7db3ed177b34c094c1a594ebbe0fe820a1eba2
Mateus Santiago <mateus_santiago2.3@outlook.com>
2022-12-14 12:18:01 -0300

    feat(documentary): implemented repository methods

--- src/documentary/documentary.repository.ts
+++ src/documentary/documentary.repository.ts
@@ -1,7 +1,31 @@
-import { Injectable } from "@nestjs/common";
-import { PrismaService } from "src/prisma/prisma.service";
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { DocumentaryEntity } from './entities/documentary.entity';
 
 @Injectable()
 export class DocumentaryRepository {
-    constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService) {}
+
+  create(data: DocumentaryEntity): Promise<DocumentaryEntity> {
+    return this.prisma.documentary.create({ data });
+  }
+
+  findAll(): Promise<DocumentaryEntity[]> {
+    return this.prisma.documentary.findMany();
+  }
+
+  findOne(id: string): Promise<DocumentaryEntity> {
+    return this.prisma.documentary.findUnique({ where: { id } });
+  }
+
+  update(
+    id: string,
+    data: Partial<DocumentaryEntity>,
+  ): Promise<DocumentaryEntity> {
+    return this.prisma.documentary.update({ where: { id }, data });
+  }
+
+  delete(id: string): Promise<DocumentaryEntity> {
+    return this.prisma.documentary.delete({ where: { id } });
+  }
 }

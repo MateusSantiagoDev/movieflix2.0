8f2179b78a96da780b008a0705ce28e1e0098698
Mateus Santiago <mateus_santiago2.3@outlook.com>
2022-12-14 10:33:53 -0300

    feat(serie): implemented 'dto' and 'entity'

---
+++ src/serie/dto/create-serie.dto.ts
@@ -0,0 +1,33 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsPositive, IsString, IsUrl } from 'class-validator';
+
+export class CreateSerieDto {
+  @IsString()
+  @ApiProperty({
+    description: 'Titulo do serie',
+    example: 'the walking dead',
+  })
+  title: string;
+
+  @IsString()
+  @ApiProperty({
+    description: 'Descrição dop serie',
+    example: 'serie de aventura',
+  })
+  description: string;
+
+  @IsNumber()
+  @IsPositive()
+  @ApiProperty({
+    description: 'Avaliação do serie',
+    example: 9.5,
+  })
+  evaluation: number;
+
+  @IsUrl()
+  @ApiProperty({
+    description: 'Iagem do serie',
+    example: 'https://lumiere-a.akamaihd.net/v1/images/398dbf5d3028f1e455b5095ea520edbb_4096x2732_e69b917e.jpeg?region=682,0,2732,2732',
+ })
+  image: string;
+}
---
+++ src/serie/entities/serie.entity.ts
@@ -0,0 +1,7 @@
+export class SerieEntity {
+  id: string;
+  title: string;
+  description: string;
+  evaluation: number;
+  image: string;
+}

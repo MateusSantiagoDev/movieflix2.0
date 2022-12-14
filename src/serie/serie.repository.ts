86a4e4051094dac27940d9c308280fed26bffe1f
Mateus Santiago <mateus_santiago2.3@outlook.com>
2022-12-14 11:04:08 -0300

    feat(serie): implemented service methods

--- src/serie/serie.repository.ts
+++ src/serie/serie.repository.ts
@@ -1,6 +1,23 @@
 import { Injectable } from "@nestjs/common";
+import { UpdateSerieDto } from "./dto/update-serie.dto";
+import { SerieEntity } from "./entities/serie.entity";
 
 @Injectable()
 export class SerieRepository {
+    delete(id: string): SerieEntity | PromiseLike<SerieEntity> {
+        throw new Error("Method not implemented.");
+    }
+    update(id: string, dto: UpdateSerieDto): Promise<SerieEntity> {
+        throw new Error("Method not implemented.");
+    }
+    findOne(id: string): Promise<SerieEntity> {
+        throw new Error("Method not implemented.");
+    }
+    findAll(): Promise<SerieEntity[]> {
+        throw new Error("Method not implemented.");
+    }
+    create(data: SerieEntity): Promise<import("./entities/serie.entity").SerieEntity> {
+        throw new Error("Method not implemented.");
+    }
     constructor() {}
 }
--- src/serie/serie.service.ts
+++ src/serie/serie.service.ts
@@ -1,23 +1,45 @@
-import { Injectable } from "@nestjs/common";
-import { CreateSerieDto } from "./dto/create-serie.dto";
-import { UpdateSerieDto } from "./dto/update-serie.dto";
+import { Injectable } from '@nestjs/common';
+import { randomUUID } from 'crypto';
+import { CreateSerieDto } from './dto/create-serie.dto';
+import { UpdateSerieDto } from './dto/update-serie.dto';
+import { SerieEntity } from './entities/serie.entity';
+import { SerieRepository } from './serie.repository';
 
 @Injectable()
 export class SerieService {
-    delete(id: string): Promise<import("./entities/serie.entity").SerieEntity> {
-        throw new Error('Method not implemented.');
-    }
-    update(id: string, dto: UpdateSerieDto): Promise<import("./entities/serie.entity").SerieEntity> {
-        throw new Error('Method not implemented.');
-    }
-    findOne(id: string): Promise<import("./entities/serie.entity").SerieEntity> {
-        throw new Error('Method not implemented.');
-    }
-    findAll(): Promise<import("./entities/serie.entity").SerieEntity[]> {
-        throw new Error('Method not implemented.');
-    }
-    create(dto: CreateSerieDto): Promise<import("./entities/serie.entity").SerieEntity> {
-        throw new Error("Method not implemented.");
-    }
-    constructor() {}
+  constructor(private readonly repository: SerieRepository) {}
+
+  create(dto: CreateSerieDto): Promise<SerieEntity> {
+    try {
+      const data: SerieEntity = { ...dto, id: randomUUID() };
+      return this.repository.create(data);
+    } catch (err) {}
+  }
+
+  findAll(): Promise<SerieEntity[]> {
+    try {
+      return this.repository.findAll();
+    } catch (err) {}
+  }
+
+  findOne(id: string): Promise<SerieEntity> {
+    try {
+      return this.repository.findOne(id);
+    } catch (err) {}
+  }
+
+  async update(id: string, dto: UpdateSerieDto): Promise<SerieEntity> {
+    try {
+      await this.findOne(id);
+      const data: Partial<SerieEntity> = { ...dto };
+      return this.repository.update(id, data);
+    } catch (err) {}
+  }
+
+  async delete(id: string): Promise<SerieEntity> {
+    try {
+      await this.findOne(id);
+      return this.repository.delete(id);
+    } catch (err) {}
+  }
 }

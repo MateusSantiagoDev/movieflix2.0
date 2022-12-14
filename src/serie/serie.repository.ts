ec299bf52c954f36eb25661d6a7662f384a6c21c
Mateus Santiago <mateus_santiago2.3@outlook.com>
2022-12-14 11:10:46 -0300

    feat(serie): implemented repository methods

--- src/serie/serie.repository.ts
+++ src/serie/serie.repository.ts
@@ -1,23 +1,28 @@
-import { Injectable } from "@nestjs/common";
-import { UpdateSerieDto } from "./dto/update-serie.dto";
-import { SerieEntity } from "./entities/serie.entity";
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { SerieEntity } from './entities/serie.entity';
 
 @Injectable()
 export class SerieRepository {
-    delete(id: string): SerieEntity | PromiseLike<SerieEntity> {
-        throw new Error("Method not implemented.");
-    }
-    update(id: string, dto: UpdateSerieDto): Promise<SerieEntity> {
-        throw new Error("Method not implemented.");
-    }
-    findOne(id: string): Promise<SerieEntity> {
-        throw new Error("Method not implemented.");
-    }
-    findAll(): Promise<SerieEntity[]> {
-        throw new Error("Method not implemented.");
-    }
-    create(data: SerieEntity): Promise<import("./entities/serie.entity").SerieEntity> {
-        throw new Error("Method not implemented.");
-    }
-    constructor() {}
+  constructor(private readonly prisma: PrismaService) {}
+
+  create(data: SerieEntity): Promise<SerieEntity> {
+    return this.prisma.serie.create({ data });
+  }
+
+  findAll(): Promise<SerieEntity[]> {
+    return this.prisma.serie.findMany();
+  }
+
+  findOne(id: string): Promise<SerieEntity> {
+    return this.prisma.serie.findFirstOrThrow({ where: { id } });
+  }
+
+  update(id: string, data: Partial<SerieEntity>): Promise<SerieEntity> {
+    return this.prisma.serie.update({ where: { id }, data });
+  }
+
+  delete(id: string): Promise<SerieEntity> {
+    return this.prisma.serie.delete({ where: { id } });
+  }
 }

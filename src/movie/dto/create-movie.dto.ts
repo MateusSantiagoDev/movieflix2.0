857eb9e986f2016ad82a4399614d9b8474daee88
Mateus Santiago <mateus_santiago2.3@outlook.com>
2022-12-13 22:16:25 -0300

    fix: update movie method

--- src/movie/dto/create-movie.dto.ts
+++ src/movie/dto/create-movie.dto.ts
@@ -1,24 +1,30 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsPositive, IsString, IsUrl } from 'class-validator';
 
 export class CreateMovieDto {
+  @IsString()
   @ApiProperty({
     description: 'Titulo do filme',
     example: 'Piratas do caribe',
   })
   title: string;
 
+  @IsString()
   @ApiProperty({
     description: 'Descrição dop filme',
     example: 'Filme de aventura',
   })
   description: string;
 
+  @IsNumber()
+  @IsPositive()
   @ApiProperty({
     description: 'Avaliação do filme',
     example: 9.5,
   })
   evaluation: number;
 
+  @IsUrl()
   @ApiProperty({
     description: 'I,agem do filme',
     example:
